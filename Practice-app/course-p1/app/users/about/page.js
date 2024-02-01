781ff6cb2e25bc65aa234f86b3694f903637eb13
Maharshi996 <raghavmanubrolu.9966@gmail.com>
2024-02-01 09:21:32 +0530

    initial commit - learned file routing

---
+++ Practice-app/course-p1/app/users/about/page.js
@@ -0,0 +1,7 @@
+const about = ()=> {
+  return (
+    <h2>this is about page</h2>
+  )
+}
+
+export default about 
---
+++ Practice-app/course-p1/app/users/page.js
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <h2>This is Users page</h2>
+  )
+}
+
+export default page

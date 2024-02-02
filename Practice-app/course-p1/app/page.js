11c1cff90dddbc71d76b81d4a92acabb0fb6c66d
Maharshi996 <raghavmanubrolu.9966@gmail.com>
2024-02-02 08:33:53 +0530

    used UserRouter hook for routing

--- Practice-app/course-p1/app/page.js
+++ Practice-app/course-p1/app/page.js
@@ -1,6 +1,20 @@
+"use client"
+import { useRouter } from "next/navigation"
+
 const home = ()=> {
+  
+  //using router for navigation
+  const router = useRouter()
+  const navigate = (page)=>{
+    router.push(page)
+  }
   return (
+    <div>
     <h1>This is home page</h1>
+    <button onClick={()=>router.push("users/about")}>go To About</button> //using arrow function inline
+    <button onClick={()=>navigate("users")}>go To Users</button> //using seperate function
+    </div>
+    
   )
 }
  
